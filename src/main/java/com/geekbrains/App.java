47db9e6d4efa50e42854c19f682ec38e19bc0c35
AleksandrBobrovskikh <bobrosan@gmail.com>
2022-05-11 21:53:11 +0300

    ДЗ от Бобровских А.В. курс 2 урок 5

--- src/main/java/com/geekbrains/App.java
+++ src/main/java/com/geekbrains/App.java
@@ -1,8 +1,55 @@
 package com.geekbrains;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("g");
+import java.util.Arrays;
 
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        firstMethod();
+        secondMethod();
+    }
+
+    private static void secondMethod() throws InterruptedException {
+        int size = 10_000_000;
+        int half = size / 2;
+        float[] arr = new float[size];
+        Arrays.fill(arr, 1.0f);
+        long startTime = System.currentTimeMillis();
+        float[] leftHalf = new float[half];
+        float[] rightHalf = new float[half];
+        System.arraycopy(arr, 0, leftHalf, 0, half);
+        System.arraycopy(arr, half, rightHalf, 0, half);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < leftHalf.length; i++) {
+                leftHalf[i] = (float) (leftHalf[i] * Math.sin(0.2f + i / 5f) * Math.cos(0.2f + i / 5f) * Math.cos(0.4f + i / 2f));
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < rightHalf.length; i++)
+                rightHalf[i] = (float) (rightHalf[i] * Math.sin(0.2f + i / 5f) * Math.cos(0.2f + i / 5f) * Math.cos(0.4f + i / 2f));
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        float[] goodArr = new float[size];
+        System.arraycopy(leftHalf, 0, goodArr, 0, half);
+        System.arraycopy(rightHalf, 0, goodArr, half, half);
+        System.out.println("Two thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
+
+    }
+
+    private static void firstMethod() {
+        int size = 10_000_000;
+        float[] arr = new float[size];
+        Arrays.fill(arr, 1.0f);
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5f) * Math.cos(0.2f + i / 5f) * Math.cos(0.4f + i / 2f));
+        }
+        System.out.println("One thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
 }
